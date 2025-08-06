d44758e69f50205459d3b4f44315b3fbd9af7d6d
tasin-chowdhury <tasinchowdhury535@gmail.com>
2025-08-06 22:34:42 +0600

    BMI Calculator done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  //states of application
+
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
+  const [bmi, setBmi] = useState("");
+  const [massage, setMassage] = useState("");
+
+  //logics of application
+
+  let calcBmi = (e) => {
+    e.preventDefault();
+    if (height === 0 || weight === 0) {
+      alert("Please enter valid height and weight");
+    } else {
+      let bmi = (weight / (height * height)) * 703;
+      setBmi(bmi.toFixed(1));
+
+      if (bmi < 25) {
+        setMassage("You are underweight");
+      } else if (bmi >= 25 && bmi < 30) {
+        setMassage("You have a healthy weight");
+      } else {
+        setMassage("You are overweight");
+      }
+    }
+  };
+
+  let reload = () => {
+    window.location.reload();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ width: "50%" }}
+      className="container  mx-auto text-center shadow p-2 m-5 bg-body-tertiary rounded"
+    >
+      <div className="row justify-content-center">
+        <form className="col-6" onSubmit={calcBmi}>
+          <h2 className="text-primary">BMI Calculator</h2>
+          <div className="m-1">
+            <label className="form-label">Enter your weight (lbs)</label>
+            <br />
+            <input
+              className="form-control"
+              type="text"
+              value={weight}
+              placeholder="Enter your weight"
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <div className="m-1">
+            <label className="form-label">Enter your height (in)</label>
+            <br />
+            <input
+              className="form-control"
+              type="text"
+              value={height}
+              placeholder="Enter your height"
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+            <button
+              className="m-1 btn btn-outline-secondary"
+              onClick={reload}
+              type="submit"
+            >
+              Reload
+            </button>
+          </div>
+          <div>
+            <h4>
+              <b>Your BMI is: {bmi}</b>
+            </h4>
+            <h5>{massage}</h5>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
